a81d05f9e1d89b79538287c1d96e199d8a06af18
Pradan-9928 <rpradan9928@gmail.com>
2026-09-16 20:57:40 +0530

    Basics of C including variable Data Types

---
+++ SEPTEMBER_13TH.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    printf("Enter First Number");
+    scanf("%d",&a);
+    printf("Enter Second Number");
+    scanf("%d",&b);
+    printf("Sum of the numbers is %d",a+b);
+    return 0;
+}

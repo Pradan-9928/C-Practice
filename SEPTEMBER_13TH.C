8cf6a6eecbbfef1ebdf9e56816154fa9d2576cbd
Pradan-9928 <rpradan9928@gmail.com>
2026-09-16 21:09:03 +0530

    These are Basic Problems of C

--- SEPTEMBER_13TH.C
+++ SEPTEMBER_13TH.C
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a;
+    /*int a;
     int b;
     printf("Enter First Number");
     scanf("%d",&a);
@@ -9,4 +9,15 @@ int main() {
     scanf("%d",&b);
     printf("Sum of the numbers is %d",a+b);
     return 0;
+
+    int length;
+    printf("Length of the side of square");
+    scanf("%d",&length);
+    printf("Area of the square is %d",length*length);*/
+    int radius;
+    printf("Radius of the circle");
+    scanf("%f",&radius);
+    printf("Area of the circle %f",3.14*radius*radius);
+    return 0;
+
 }
